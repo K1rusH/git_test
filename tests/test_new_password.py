bfc703108700ca20994da5a928998aa848c85d32
Viberr <sashatarynin@gmail.com>
2024-12-01 17:17:00 +0300

    Добавление Тестов при написании пароля

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -8,13 +8,26 @@ def test_password_characters():
     for char in password:
         assert char in valid_characters
 
-"""
-Допиши еще один тест из предложенных. Или придумай свой.
-Если сможешь написать больше, то будет круто!
-
-Тест, что длина пароля соответствует заданной
-Тест, что два сгенерированных подряд пароля различаются
-"""
-def test_password_dlina():
+def test_password_length():
+    """Тест, что длина пароля соответствует заданной"""
     for i in range(1, 25):
         assert len(generate_password(length=i)) == i
+
+def test_password_uniqueness():
+    """Тест, что два сгенерированных подряд пароля различаются"""
+    password1 = generate_password(12)
+    password2 = generate_password(12)
+    assert password1 != password2
+
+def test_password_min_length():
+    """Тест на минимальную длину пароля"""
+    password = generate_password(length=8)
+    assert len(password) >= 8
+
+def test_password_includes_characters():
+    """Тест, что пароль содержит хотя бы один символ каждого типа"""
+    password = generate_password(12)
+    assert any(c.islower() for c in password), "Пароль должен содержать строчные буквы"
+    assert any(c.isupper() for c in password), "Пароль должен содержать прописные буквы"
+    assert any(c.isdigit() for c in password), "Пароль должен содержать цифры"
+    assert any(c in string.punctuation for c in password), "Пароль должен содержать специальные символы"
